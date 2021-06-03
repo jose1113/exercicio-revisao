e4e9202274f9b23ab2087e9dda299a8f3a5f97bc
jose1113 <joseigor113@gmail.com>
2021-06-03 12:39:46 -0300

    create Menu component

---
+++ components/Menu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">Revisão</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" class="d-flex justify-content-center text-white" is-nav>
+      <b-navbar-nav>
+        <b-nav-item>
+          <nuxt-link to="#">
+            Réplica do Instagram
+          </nuxt-link>
+        </b-nav-item>
+        <b-nav-item>
+          <nuxt-link to="#">
+            Formulário de login
+          </nuxt-link>
+        </b-nav-item>
+        <b-nav-item>
+          <nuxt-link to="#">
+            Formulário de endereço
+          </nuxt-link>
+        </b-nav-item>
+        <b-nav-item>
+          <nuxt-link to="#">
+            Formulário de endereço 2
+          </nuxt-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
